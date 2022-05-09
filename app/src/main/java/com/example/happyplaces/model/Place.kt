c8954ff631b72b4656741c536f211bd2044d37eb
Alex Uioreanu <alexandru.uioreanu@gmail.com>
2022-05-09 11:01:21 +0300

    Update 2.0

--- app/src/main/java/com/example/happyplaces/model/Place.kt
+++ app/src/main/java/com/example/happyplaces/model/Place.kt
@@ -5,21 +5,21 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Place(
-
     @PrimaryKey(autoGenerate = true) var id: Int,
-
     var name: String,
     var destination: String,
     var activityType: String,
     var placeType: String,
     var dateTime: String,
     var note: String,
-    var isFavorite: Boolean,
+    var isFavorite: Boolean?,
     var image: String,
     var overallMood: Int,
-    var lat: Double,
-    var long: Double
-){
-    constructor() : this(0,"","","","","","",false,"",0,0.0,0.0)
-    constructor(name: String, destination: String, activityType: String, placeType: String, note: String, date: String, mood: Int, image: String) : this()
+    var lat: Double?,
+    var long: Double?
+) {
+//    constructor(name: String,destination: String,activityType: String,placeType: String,dateTime: String,note: String,image: String,overallMood: Int) : this(name,destination,activityType,placeType,dateTime,note,image,overallMood){
+//
+//    }
+
 }
